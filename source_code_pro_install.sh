e7f94afbb0e7475090d73404c5d72fbfaaf85563
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2016-07-08 11:08:42 +0800

    Clean up script

--- source_code_pro_install.sh
+++ source_code_pro_install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+
 FONT_NAME="SourceCodePro"
 URL="https://github.com/adobe-fonts/source-code-pro/archive/1.017R.tar.gz"
 
 mkdir /tmp/$FONT_NAME
 cd /tmp/$FONT_NAME
-wget $URL -O "`echo $FONT_NAME`.tar.gz"
+wget $URL -O ${FONT_NAME}.tar.gz
 tar --extract --gzip --file ${FONT_NAME}.tar.gz
-sudo mkdir /usr/share/fonts/truetype/$FONT_NAME
-sudo cp -rf /tmp/$FONT_NAME/. /usr/share/fonts/truetype/$FONT_NAME/.
+sudo mkdir /usr/share/fonts/truetype/${FONT_NAME}
+sudo cp -rf /tmp/${FONT_NAME}/. /usr/share/fonts/truetype/${FONT_NAME}/.
 fc-cache -f -v
