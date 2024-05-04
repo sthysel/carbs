96fde2aa2d71cc6ac02a6d89b1a40d4ff5048bee
sthysel <sthysel@gmail.com>
2024-05-04 22:38:00 +0800

    random: so many updates

--- scripts/smart-rename.sh
+++ scripts/smart-rename.sh
@@ -1,22 +1,36 @@
 #!/bin/bash
 
-if [ -z "$1" ]
-then
-  echo "Usage: $0 ~/path/to/image/directory"
-  echo "Example: $0 ."
-  exit 1
+if [ -z "$1" ]; then
+	echo "Usage: $0 ~/path/to/image/directory"
+	echo "Example: $0 ."
+	exit 1
 fi
 
-cd "$1" || exit 1
+IMAGE_DIR=$1
+# Create the 'renamed' directory if it does not exist
+RENAME_DIR="$IMAGE_DIR/renamed"
+mkdir -p "$RENAME_DIR"
 
-for img_file in *.png *.jpg *.jpeg *.wepp
-do
-  [ -f "$img_file" ] || continue
+cd "$IMAGE_DIR" || exit 1
 
-  extension="${img_file##*.}"
-  caption=$(blip-caption --large "$img_file")
-  new_filename=$(echo "$caption" | tr ' ' '-' | sed "s/[^a-zA-Z0-9-]//g").$extension
-  echo "$img_file" "$new_filename"
-  # mv "$img_file" "$new_filename"
+for img_file in *.png *.jpg *.jpeg *.wepp; do
+	[ -f "$img_file" ] || continue
+
+	extension="${img_file##*.}"
+	caption=$(blip-caption --large "$img_file")
+	new_filename=$(echo "$caption" | tr ' ' '-' | sed "s/[^a-zA-Z0-9-]//g").$extension
+	echo "$img_file -> $new_filename"
+	mv "$img_file" "$RENAME_DIR/$new_filename"
 done
 
+delete_small_images() {
+	find . -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" \) -exec sh -c '                                         ─╯
+  for img; do
+    read width height < <(identify -format "%w %h" "$img")
+    if [ "$width" -lt 1920 ] || [ "$height" -lt 1080 ]; then
+      echo "Deleting $img, size: ${width}x${height}"
+      rm "$img"
+    fi
+  done
+' sh {} +
+}
