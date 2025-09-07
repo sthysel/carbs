1c3ca8e27e3f0e93267b58fca784b59af989431b
sthysel <sthysel@gmail.com>
2025-09-07 16:29:44 +0800

    beets fuxors

--- dotfiles/nvim/.config/nvim/lua/config/options.lua
+++ dotfiles/nvim/.config/nvim/lua/config/options.lua
@@ -2,20 +2,19 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 --
-
--- OSC52 clipboard for SSH sessions
+-- OSC 52 clipboard configuration for SSH sessions
 if os.getenv("SSH_TTY") then
-  vim.g.clipboard = {
-    name = "OSC 52",
-    copy = {
-      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-    },
-    paste = {
-      ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-      ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
-    },
-  }
+    vim.g.clipboard = {
+        name = 'OSC 52',
+        copy = {
+            ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+            ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+        },
+        paste = {
+            ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+            ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+        },
+    }
 end
 
 -- Enable unnamedplus to make yank operations use system clipboard
