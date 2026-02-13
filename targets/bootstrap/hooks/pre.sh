46f9c8e012eaf640ca1fc96f2b4458fc037a55c2
sthysel <sthysel@gmail.com>
2026-02-13 11:05:05 +0800

    feat: adds unzip, propably yazi plugin update

--- targets/bootstrap/hooks/pre.sh
+++ targets/bootstrap/hooks/pre.sh
@@ -23,42 +23,43 @@ ARCH_PACKAGES="
     trash-cli
     tree
     wget
+    unzip
 "
 
 install_uv() {
-    if ! has uv; then
-        curl -LsSf https://astral.sh/uv/install.sh | sh
-    fi
+  if ! has uv; then
+    curl -LsSf https://astral.sh/uv/install.sh | sh
+  fi
 }
 
 arch() {
-    # the very basics needed to bootstrap
-    rootdo pacman -S --needed --noconfirm base-devel jq curl git
+  # the very basics needed to bootstrap
+  rootdo pacman -S --needed --noconfirm base-devel jq curl git
 
-    # yay, to install the good shit
-    if ! has yay; then
-        git clone https://aur.archlinux.org/yay.git yay
-        makepkg -si --noconfirm
-        yay -S yay
-    fi
+  # yay, to install the good shit
+  if ! has yay; then
+    git clone https://aur.archlinux.org/yay.git yay
+    makepkg -si --noconfirm
+    yay -S yay
+  fi
 
-    # install all the essentials
-    # shellcheck disable=SC2086
-    yay_install $ARCH_PACKAGES
+  # install all the essentials
+  # shellcheck disable=SC2086
+  yay_install $ARCH_PACKAGES
 
-    # uv cause that's where its at
-    install_uv
+  # uv cause that's where its at
+  install_uv
 }
 
 steamos() {
-    rootdo steamos-readonly disable
-    rootdo pacman-key --init
-    rootdo pacman-key --populate archlinux
-    rootdo pacman-key --populate holo
-    rootdo pacman -S --noconfirm jq git curl
+  rootdo steamos-readonly disable
+  rootdo pacman-key --init
+  rootdo pacman-key --populate archlinux
+  rootdo pacman-key --populate holo
+  rootdo pacman -S --noconfirm jq git curl
 }
 
 debian() {
-    rootdo apt install -y jq curl git
-    install_uv
+  rootdo apt install -y jq curl git
+  install_uv
 }
